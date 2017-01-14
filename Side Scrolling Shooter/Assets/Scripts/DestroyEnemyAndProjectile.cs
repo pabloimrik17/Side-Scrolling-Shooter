9ba4f80d49b5dea6b8fe4ec519e2e197322f4dc1
Pablo <pabloimrik17@gmail.com>
2017-01-14 14:22:46 +0100

    Parte 2

---
+++ Side Scrolling Shooter/Assets/Scripts/DestroyEnemyAndProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemyAndProjectile : MonoBehaviour {
+
+    public GameObject explosionParticle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Collision") {
+            spawnExplosionParticle();
+            Destroy(collision.gameObject);
+        }
+    } 
+
+    void spawnExplosionParticle() {
+        Instantiate(explosionParticle, new Vector3(transform.position.x, transform)
+    }
+}
---
+++ Side Scrolling Shooter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyToSpawn;
+
+    public bool canSpawn = true;
+
+    public float enemySpawnTime = 1.2f;
+    public float maxSpawnY = 8.0f;
+    public float startingX = 25.0f;
+
+    float randomY = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(enemySpawnTimer());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator enemySpawnTimer() {
+        while(canSpawn) {
+            spawnEnemy();
+            yield return new WaitForSeconds(enemySpawnTime);
+        }
+    }
+
+    void spawnEnemy() {
+        randomY = Random.Range(-maxSpawnY, maxSpawnY);
+        Instantiate(enemyToSpawn, new Vector3(startingX, randomY, 0), Quaternion.identity);
+    }
+}
